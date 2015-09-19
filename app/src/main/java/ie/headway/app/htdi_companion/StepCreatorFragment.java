6f4a8dee4b5cc6ad9cd2436201504c6a0d8e62c9
abuicke <abuicke@treemetrics.com>
2015-09-19 13:58:25 +0100

    Working in principal

--- app/src/main/java/ie/headway/app/htdi_companion/StepCreatorFragment.java
+++ app/src/main/java/ie/headway/app/htdi_companion/StepCreatorFragment.java
@@ -5,6 +5,7 @@ import android.app.Fragment;
 import android.content.res.Resources;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,6 +16,9 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
+
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -37,6 +41,12 @@ public class StepCreatorFragment extends Fragment {
 	private CameraView mImage;
 
     private Button mCreateStepButton;
+
+    final File file = new File(
+            Environment.getExternalStorageDirectory() +
+                    File.separator + "Headway" +
+                    File.separator + "TestCompanion" +
+                    File.separator + "1.jpg");
 
 	public static final StepCreatorFragment newInstance() {
 		final StepCreatorFragment stepCreatorFragment = new StepCreatorFragment();
@@ -63,8 +73,16 @@ public class StepCreatorFragment extends Fragment {
         mCreateStepButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-        		final Step step = new Step(mText.getText().toString(), "", "");
                 mImage.captureImage();
+        		final Step step = new Step(mText.getText().toString(), file.getAbsolutePath(), "");
+
+                final Serializer serializer = new Persister();
+
+                try {
+                    serializer.write(step, new File(file.getParent() + File.separator + "test.xml"));
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
 
@@ -89,7 +107,7 @@ public class StepCreatorFragment extends Fragment {
 		final Resources resources = activity.getResources();
 		final DisplayMetrics displayMetrics = resources.getDisplayMetrics();
 		final float scale = displayMetrics.density;
-		int px = (int) (dp * scale + 0.5f);
+		final int px = (int) (dp * scale + 0.5f);
 		return px;
 	}
 
@@ -98,7 +116,7 @@ public class StepCreatorFragment extends Fragment {
 			@Override
 			public void onPictureTaken(byte[] data, Camera camera) {
 				try {
-					writeRawDataToFile(data, new File("/sdcard/img"));
+					writeRawDataToFile(data, file);
 				} catch (IOException e) {
 					Log.e(TAG, "exception on line: writeRawDataToFile(data, file)", e);
 				}
